106b848259ac8fe7e3c6828e9a08c5356a23293c
Karoline Lima <karol.lima012@gmail.com>
2021-09-06 21:54:06 -0300

    configuration database credentials

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    port: '5438',
-    password:'postgres',
-    database:'',
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
     define: {
         timeStamps:true,
         underscored: true,
