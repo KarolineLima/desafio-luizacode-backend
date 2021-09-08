a86dec89bd414fc49cd2aa4bcec60d2fd43071f3
capinarei <juliana.monte@outlook.com>
2021-09-08 17:14:07 -0400

    atualização routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,9 @@
 import { Router } from 'express';
 
+import StoreController from './app/controller/StoreController';
+
 const routes = new Router();
+
+routes.get('/stores', StoreController.find);
+
+export default routes;
