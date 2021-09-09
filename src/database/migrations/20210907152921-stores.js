8e33559f03bce54f98efeade330a7e4939f3034d
Dani Botelho <danibl26@gmail.com>
2021-09-08 21:41:47 -0300

    fixed table orders/stores

--- src/database/migrations/20210907152921-stores.js
+++ src/database/migrations/20210907152921-stores.js
@@ -25,11 +25,20 @@ module.exports = {
       address:{
         type: Sequelize.STRING,
         allowNull: false,
-      }
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     }); 
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: queryInterface => {
     return queryInterface.dropTable('stores');
   }
+
 };
