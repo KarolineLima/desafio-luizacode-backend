bd78de0c76f0c4b9b328e8331dbcf16514134ff3
Elisama Santos <35153550+ElisamaSS@users.noreply.github.com>
2021-09-10 20:50:18 -0300

    work in progress

--- src/app/controller/OrderController.js
+++ src/app/controller/OrderController.js
@@ -5,29 +5,18 @@ import User from '../models/User';
 import Store from '../models/Store';
 import Product from '../models/Product';
 import Order from '../models/Order';
+import { response } from 'express';
 
-/*
-numberOrder = número do pedido
-optionPickup = opção de retirada
-shipping = opção de envio (frete)
-orderTotal = total de pedidos
-
-/path1/path2
-id_product/id_user
-- pedido
-- usuario
-- venddor
-
-*/
 class OrderController{
   
     async createOrder(req, res){
-        const product= ["123", "568"];
+        const orders= ["123", "568"];
 
         function products(item){
             if(item>0){
             const colunaTabelaUsario =+ item
-            console.log("codigo", colunaTabelaUsario);
+            // console.log("codigo", colunaTabelaUsario);
+            
             const schema = Yup.object().shape({
                 numberOrder: Yup.number(),
                 id_product: Yup.number().required(),
@@ -44,22 +33,39 @@ class OrderController{
                 return res.status(401).json({ message: 'Opa, dados inválidos'})
     
             }
-
             function randomNumber(){
                 const numberOrder = Math.floor(Math.random() * 65536789);
                 return numberOrder
             }
             
 
-            const {randomNumber, id_product, optionPickup, shipping, orderTotal,status, id_store, id_user } = await User.create(req.body);
-            return res.json({randomNumber, id_product, optionPickup, shipping, orderTotal,status, id_store, id_user });
+            const product = await Product.findOne({ 
+                where: { id_product: id_product, id_product: true }
+            });
+
+            if(product){
+                if(product.quantity >= 1){
+                    const {randomNumber, id_product, optionPickup, shipping, orderTotal,status, id_store, id_user } = await orders.create(req.body);
+                    console.log(randomNumber, id_product, optionPickup, shipping, orderTotal,status, id_store, id_user)
+                    return res.json({ message: "é possível comprar produto"})
+
+                }else{
+                    return res.json({ message: "produto esgotado"})
+                }
+                
+            }
+      
+
             
-            // const newRequest = await OrderController.findById(req.params.id_user, req.params.id_product);
+
+            
+            
+        
             }
             return 0
         }
             
-        product.forEach(products);
+        orders.forEach(products);
             
     }
 
