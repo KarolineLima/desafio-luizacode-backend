f55aac929d6ccb7ab0752d48cb46ffa216c10310
capinarei <juliana.monte@outlook.com>
2021-09-11 16:38:18 -0400

    Changes to product model

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,23 +1,31 @@
 import Sequelize, { Model } from 'sequelize';
 
 class Product extends Model {
-  static init(sequelize) {
-    super.init({
-      name: Sequelize.STRING,
-      valueProduct: Sequelize.INTEGER,
-      quantity: Sequelize.INTEGER,
-    }, 
-    {  
-      sequelize, 
-    });
-   
-    return this;
-  }
+    static init(sequelize) {
+        super.init(
+            {
+                name: Sequelize.STRING,
+                value_product: {
+                    type: Sequelize.INTEGER,
+                    field: 'valueProduct',
+                },
+                quantity: Sequelize.INTEGER,
+            },
+            {
+                sequelize,
+            }
+        );
 
-  static associate(models){
-    this.belongsTo( models.Category, { foreignKey: 'category_id', as: 'category'});
-    this.belongsTo( models.Image, { foreignKey: 'image_id', as: 'image'});
-  }
+        return this;
+    }
+
+    static associate(models) {
+        this.belongsTo(models.Category, {
+            foreignKey: 'category_id',
+            as: 'category',
+        });
+        // this.belongsTo( models.Image, { foreignKey: 'image_id', as: 'image'});
+    }
 }
 
-export default Product
+export default Product;
