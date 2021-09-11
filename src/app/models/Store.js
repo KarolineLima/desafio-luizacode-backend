b214481f7353e34fa04f6296432fc32c0085a631
capinarei <juliana.monte@outlook.com>
2021-09-11 16:40:25 -0400

    fixes Store

--- src/app/models/Store.js
+++ src/app/models/Store.js
@@ -2,35 +2,19 @@ import Sequelize, { Model } from 'sequelize';
 import bcrypt from 'bcryptjs';
 
 class Store extends Model {
-  static init(sequelize) {
-    super.init({
-      id_store:{
-        primaryKey: true,
-        allowNull: false,
-        field: 'id_store',
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-      },
-      name_store: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password: Sequelize.VIRTUAL,
-      password_hash: Sequelize.STRING,
-      address: Sequelize.STRING,
-    }, 
-    {  
-      sequelize, 
-    });
-    this.addHook('beforeSave', async store => {
-      if(store.password){
-        store.password_hash = await bcrypt.hash(store.password, 10)
-      }
-    })
-    return this;
-  }
+    static init(sequelize) {
+        super.init(
+            {
+                name: Sequelize.STRING,
+                address: Sequelize.STRING,
+            },
+            {
+                sequelize,
+            }
+        );
 
-  checkPassword(password){
-    return bcrypt.compare(password, this.password_hash)
-  }
+        return this;
+    }
 }
 
-export default Store
+export default Store;
