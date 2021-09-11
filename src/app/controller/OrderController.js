e73f08492ca17ae57872644ba703f43178a404eb
capinarei <juliana.monte@outlook.com>
2021-09-11 16:37:40 -0400

    Fixes to Order Model & Controller

--- src/app/controller/OrderController.js
+++ src/app/controller/OrderController.js
@@ -4,100 +4,102 @@ import Product from '../models/Product';
 import Order from '../models/Order';
 
 class OrderController {
-
     async createOrder(req, res) {
-
         function randomNumber() {
             const numberOrder = Math.floor(Math.random() * 65536789);
-            return numberOrder
+            return numberOrder;
         }
-        console.log(randomNumber());
 
         const schema = Yup.object().shape({
             numberOrder: Yup.number(),
-            id_product: Yup.number().required(),
+            product_id: Yup.number().required(),
             optionPickup: Yup.string(),
             shipping: Yup.number(),
             orderTotal: Yup.number().required(),
             status: Yup.string().required(),
-            store: Yup.number().required(),
-            user: Yup.number().required()
-
+            store_id: Yup.number().required(),
+            user_id: Yup.number().required(),
         });
 
-        //console.log("schema", schema);
-        //console.log("aqui", id_product);
-
+        const { product_id, store_id, user_id } = req.body;
 
         if (!(await schema.isValid(req.body))) {
-            console.log("body", req.body)
-            return res.status(401).json({ message: 'Opa, dados inválidos' })
-
+            console.log('body', req.body);
+            return res.status(401).json({ message: 'Opa, dados inválidos' });
         }
-        const orders = ["123", "568"];
+        const orders = [];
 
         async function products(item) {
             if (item > 0) {
-                const colunaTabelaUsario = + item
-                console.log("codigo", colunaTabelaUsario);
+                const colunaTabelaUsario = +item;
 
-
-                const { id_product } = await Product.findOne({
-                    where: { id_product: id_product, id_product: true }
+                const product = await Product.findOne({
+                    where: { id: product_id },
                 });
-
                 const store = await Store.findOne({
-                    where: { id_product: id_product, id_product: true, $and: { id_store: id_store, id_store: true } }
+                    where: {
+                        id: store_id,
+                    },
                 });
 
-                console.log(product, store)
+                console.log(product, store);
 
                 if (store) {
                     if (product) {
                         if (product.quantity >= 1) {
-                            const { randomNumber, id_product, optionPickup, shipping, orderTotal, status, id_store, id_user } = await Order.create(req.body);
-                            Product.update(
-                                { quantity: product.quantity - 1 },
-                            ).then((updatedRows) => {
+                            const {
+                                randomNumber,
+                                id_product,
+                                optionPickup,
+                                shipping,
+                                orderTotal,
+                                status,
+                                id_store,
+                                id_user,
+                            } = await Order.create(req.body);
+                            Product.update({
+                                quantity: product.quantity - 1,
+                            }).then((updatedRows) => {
                                 res.json(updatedRows);
-                            })
-                            return res.json({ message: "é possível comprar produto" })
-
-
+                            });
+                            return res.json({
+                                message: 'é possível comprar produto',
+                            });
                         } else {
-                            return res.json({ message: "produto esgotado" })
+                            return res.json({ message: 'produto esgotado' });
                         }
-
                     } else {
-                        return res.json({ message: "Produto inexistente" })
+                        return res.json({ message: 'Produto inexistente' });
                     }
-
                 } else {
-                    return res.json({ message: "Loja inexistente ou produto inexistente" })
+                    return res.json({
+                        message: 'Loja inexistente ou produto inexistente',
+                    });
                 }
-
             }
-            return 0
+            return 0;
         }
 
         orders.forEach(products);
-
     }
 
     async removeProduct(req, res) {
-        const orders = ["123", "568"];
-        const index = product.indexOf(req.body)
-        const option = orders.splice(index, 1)
+        const orders = [];
+        const index = product.indexOf(req.body);
+        const option = orders.splice(index, 1);
 
-        return res.status(200).json({ message: `Produto removido: ${option}. Lista de produtos atualizada: ${orders}` })
+        return res.status(200).json({
+            message: `Produto removido: ${option}. Lista de produtos atualizada: ${orders}`,
+        });
     }
 
     async addProduct(req, res) {
-        const orders = ["123", "568"];
-        const add_product = orders.push(req.body)
+        const orders = [];
+        const add_product = orders.push(req.body);
 
-        return res.status(200).json({ message: `Produto adicionado ${add_product}. Lista de produtos atualizada: ${orders}` })
-
+        return res.status(200).json({
+            message: `Produto adicionado ${add_product}. Lista de produtos atualizada: ${orders}`,
+        });
     }
 }
 
