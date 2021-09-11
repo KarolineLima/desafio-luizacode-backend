3a68023a8e91deaae86c6a907a5b2d39263a4445
Elisama Santos <35153550+ElisamaSS@users.noreply.github.com>
2021-09-10 22:50:05 -0300

    work in progress

---
+++ src/database/index.js
@@ -0,0 +1,24 @@
+import Sequelize from "sequelize";
+import Order from '../app/models/Order';
+import Product from "../app/models/Product";
+import User from "../app/models/User";
+import Store from '../app/models/Store';
+import databaseConfig from '../config/database';
+
+const models = [ Store, Order, Product, User ];
+
+class Database{
+  constructor(){
+    this.init()
+  }
+
+  init(){
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models))
+  }
+}
+
+export default new Database();
