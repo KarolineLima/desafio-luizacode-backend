7e87b7d37440d7e57749ebcff37a4334853a0bf2
Elisama Santos <35153550+ElisamaSS@users.noreply.github.com>
2021-09-10 21:23:05 -0300

    work in progress

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import OrderController from './app/controller/OrderController';
 
 import StoreController from './app/controller/StoreController';
 
 const routes = new Router();
 
 routes.get('/stores', StoreController.find);
+
+routes.post('/order', OrderController.createOrder);
 
 export default routes;
