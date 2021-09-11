f3fa3dfec0a3df6abccda0273a1f24d67a6f355b
Elisama Santos <35153550+ElisamaSS@users.noreply.github.com>
2021-09-10 21:23:05 -0300

    work in progress

--- src/app/controller/OrderController.js
+++ src/app/controller/OrderController.js
@@ -7,71 +7,80 @@ import Product from '../models/Product';
 import Order from '../models/Order';
 import { response } from 'express';
 
-class OrderController{
-  
-    async createOrder(req, res){
-        const orders= ["123", "568"];
+class OrderController {
 
-        function products(item){
-            if(item>0){
-            const colunaTabelaUsario =+ item
-            // console.log("codigo", colunaTabelaUsario);
-            
-            const schema = Yup.object().shape({
-                numberOrder: Yup.number(),
-                id_product: Yup.number().required(),
-                optionPickup: Yup.string(),
-                shipping: Yup.number(),
-                orderTotal: Yup.number().required(),
-                status:Yup.string().required(),
-                id_store: Yup.number().required(),
-                id_user: Yup.number().required()     
-        
-            });
+    async createOrder(req, res) {
+        const orders = ["123", "568"];
+
+        function products(item) {
+            if (item > 0) {
+                const colunaTabelaUsario = + item
+                console.log("codigo", colunaTabelaUsario);
+                
+
+                const schema = Yup.object().shape({
+                    numberOrder: Yup.number(),
+                    id_product: Yup.number().required(),
+                    optionPickup: Yup.string(),
+                    shipping: Yup.number(),
+                    orderTotal: Yup.number().required(),
+                    status: Yup.string().required(),
+                    id_store: Yup.number().required(),
+                    id_user: Yup.number().required()
+
+                });
+
+               
+                if (!(await schema.isValid(req.body))) {
+                    return res.status(401).json({ message: 'Opa, dados inválidos' })
+
+                }
+                function randomNumber() {
+                    const numberOrder = Math.floor(Math.random() * 65536789);
+                    return numberOrder
+                }
+
+
+                const product = await Product.findOne({
+                    where: { id_product: id_product, id_product: true }
+                });
+
+                const store = await Store.findOne({
+                    where: { id_product: id_product, id_product: true, $and:{id_store: id_store, id_store: true} }
+                });
+
+                console.log(product, store)
+
+                if(store){
+                    if (product) {
+                        if (product.quantity >= 1) {
+                            const { randomNumber, id_product, optionPickup, shipping, orderTotal, status, id_store, id_user } = await orders.create(req.body);
+                            Product.update(
+                                { quantity: product.quantity - 1 },
+                            ).then((updatedRows) => {
+                                res.json(updatedRows);
+                            })
+                            return res.json({ message: "é possível comprar produto" })
     
-            if(!(await schema.isValid(req.body))){
-                return res.status(401).json({ message: 'Opa, dados inválidos'})
     
-            }
-            function randomNumber(){
-                const numberOrder = Math.floor(Math.random() * 65536789);
-                return numberOrder
-            }
-            
-
-            const product = await Product.findOne({ 
-                where: { id_product: id_product, id_product: true }
-            });
-
-            if(product){
-                if(product.quantity >= 1){
-                    const {randomNumber, id_product, optionPickup, shipping, orderTotal,status, id_store, id_user } = await orders.create(req.body);
-                    Product.update(
-                        {quantity: product.quantity - 1},
-                    ).then((updatedRows) =>{
-                        res.json(updatedRows);
-                    })
-                    return res.json({ message: "é possível comprar produto"})
-
+                        } else {
+                            return res.json({ message: "produto esgotado" })
+                        }
+    
+                    }else{
+                        return res.json({ message: "Produto inexistente" })
+                    }
 
                 }else{
-                    return res.json({ message: "produto esgotado"})
+                    return res.json({ message: "Loja inexistente ou produto inexistente" })
                 }
-                
-            }
-      
-
-            
-
-            
-            
-        
+               
             }
             return 0
         }
-            
+
         orders.forEach(products);
-            
+
     }
 
 }
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+<<<<<<< HEAD
 import multer from 'multer';
 import multerConfig from './config/multer';
+=======
+import OrderController from './app/controller/OrderController';
+>>>>>>> work in progress
 
 import StoreController from './app/controller/StoreController';
 import ImageController from './app/controller/ImageController';
@@ -12,6 +16,7 @@ const upload = multer(multerConfig);
 
 routes.get('/stores', StoreController.find);
 
+<<<<<<< HEAD
 routes.post('/product', ProductController.createProduct)
 routes.get('/product', ProductController.listAll)
 routes.get('/product/:id', ProductController.listById)
@@ -21,5 +26,8 @@ routes.delete('/product/:id', ProductController.delete)
 
 routes.post('/image', upload.single('image'), ImageController.store);
 //demais rotas
+=======
+routes.post('/order', OrderController.createOrder);
+>>>>>>> work in progress
 
 export default routes;
