ef8658ca08cb5d8abc869bb9b9b65cbd91bf304a
capinarei <juliana.monte@outlook.com>
2021-09-11 16:39:23 -0400

    added body-parser to server

--- src/app.js
+++ src/app.js
@@ -8,15 +8,16 @@ import routes from './routes';
 
 import './database';
 
-class App{
-  constructor() {
-    this.server = express();
-    this.routes();
-  }
+class App {
+    constructor() {
+        this.server = express();
+        this.server.use(express.json());
+        this.routes();
+    }
 
-  routes(){
-    this.server.use(routes);
-   }
+    routes() {
+        this.server.use(routes);
+    }
 }
 
 export default new App().server;
