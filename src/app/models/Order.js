a2831474ffd2ff392ba43ec3524ad8cc14f19939
capinarei <juliana.monte@outlook.com>
2021-09-11 16:37:40 -0400

    Fixes to Order Model & Controller

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -1,27 +1,33 @@
 import Sequelize, { Model } from 'sequelize';
 
 class Order extends Model {
-  static init(sequelize) {
-    super.init({
-      numberOrder: Sequelize.STRING,
-      optionPickup: Sequelize.STRING,
-      id_product: Sequelize.INTEGER,
-      shipping: Sequelize.INTEGER,
-      orderTotal: Sequelize.INTEGER,
-      status: Sequelize.STRING,
-      store: Sequelize.INTEGER,
-      user: Sequelize.INTEGER,
-    }, {
-      sequelize,
-    });
+    static init(sequelize) {
+        super.init(
+            {
+                numberOrder: Sequelize.STRING,
+                optionPickup: Sequelize.STRING,
+                // id_product: Sequelize.INTEGER,
+                shipping: Sequelize.INTEGER,
+                orderTotal: Sequelize.INTEGER,
+                status: Sequelize.STRING,
+                // store: Sequelize.INTEGER,
+                // user: Sequelize.INTEGER,
+            },
+            {
+                sequelize,
+            }
+        );
 
-    return this;
-  }
-  static associate(models) {
-    this.belongsTo(models.Product, { foreignKey: 'id_product', as: 'products' });
-    this.belongsTo(models.Store, { foreignKey: 'id_store', as: 'stores' });
-    this.belongsTo(models.User, { foreignKey: 'id_user', as: 'users' });
-  }
+        return this;
+    }
+    static associate(models) {
+        this.belongsTo(models.Product, {
+            foreignKey: 'product_id',
+            as: 'products',
+        });
+        this.belongsTo(models.Store, { foreignKey: 'store_id', as: 'stores' });
+        this.belongsTo(models.User, { foreignKey: 'user_id', as: 'users' });
+    }
 }
 
 export default Order;
