54a225efa7298485159dd494d208595421103a56
clenia1e <cleniaguedes1@gmail.com>
2021-09-11 15:17:27 -0300

    remove conflicts with routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 import { Router } from 'express';
-<<<<<<< HEAD
 import multer from 'multer';
 import multerConfig from './config/multer';
-=======
 import OrderController from './app/controller/OrderController';
->>>>>>> work in progress
 
 import StoreController from './app/controller/StoreController';
 import ImageController from './app/controller/ImageController';
@@ -16,7 +13,6 @@ const upload = multer(multerConfig);
 
 routes.get('/stores', StoreController.find);
 
-<<<<<<< HEAD
 routes.post('/product', ProductController.createProduct)
 routes.get('/product', ProductController.listAll)
 routes.get('/product/:id', ProductController.listById)
@@ -26,8 +22,6 @@ routes.delete('/product/:id', ProductController.delete)
 
 routes.post('/image', upload.single('image'), ImageController.store);
 //demais rotas
-=======
 routes.post('/order', OrderController.createOrder);
->>>>>>> work in progress
 
 export default routes;
